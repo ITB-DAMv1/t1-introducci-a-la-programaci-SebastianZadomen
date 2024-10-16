001e398bd1b7272d37d7dd715d49008c983c65e3
SebastianZadomen <sebastian.zadomen.7e8@itb.cat>
2024-10-16 12:43:42 +0200

    T1. Prova pràctica 1 (B)

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Multiplicador
+{
+    public class Program
+    {
+        public static void Main(String[]args)
+        {
+            const string Msg = "Introduce un número";
+
+            int userNum1 = 0;
+            int userNum2 = 0;
+            int resultado = 0;
+
+            Console.WriteLine(Msg);
+            userNum1 = Convert.ToInt32(Console.ReadLine());
+            userNum2 = Convert.ToInt32(Console.ReadLine());
+
+            resultado = userNum1 * userNum2;
+            Console.WriteLine("Su respuesta es {0}", resultado);
+        }
+    }
+}
